0ac141f945bbc5f4ccf145d276ab4c93c26f601d
Alan Francis <alan.prime567@gmail.com>
2024-08-11 00:48:05 +0530

    feat:config shadcn

---
+++ app/signin/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  //   FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Nav from "@/components/ui/nav";
+import { ThemeProvider } from "@/components/context/themeProvider";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Enter a valid username",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long",
+  }),
+});
+
+function Signin() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Nav />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="flex justify-center items-center h-screen w-full">
+          <Form {...form}>
+            <form className="space-y-8 border p-8 rounded-sm min-w-[25%]">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default Signin;
---
+++ components/ui/nav.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React from "react";
+import { ThemeProvider } from "../context/themeProvider";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "./button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+
+function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function Nav() {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <nav className="flex items-center justify-between p-5 w-full border-b">
+        <div className="flex items-center space-x-4">
+          <a href="/" className="text-lg font-bold">
+            Rudiram Admin Panel
+          </a>
+        </div>
+        <div className="flex items-center space-x-4">
+          <ModeToggle />
+        </div>
+      </nav>
+    </ThemeProvider>
+  );
+}
+
+export default Nav;
